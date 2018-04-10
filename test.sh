4cdf78c7224f50a938f56c9be9c3caced4a411d3
Atsushi Watanabe <atsushi.w@ieee.org>
2018-04-10 09:37:06 +0900

    Update settings

--- test.sh
+++ test.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 sleep 1
-ogg123 -r -d alsa -o dev:plug:plugj -v /test.ogg &
+ogg123 -r -d alsa -v /mono.ogg &
 
 sleep 2
-ogg123 -r -d alsa -v -o dev:plug:plugj /test2.ogg &
+ogg123 -r -d alsa -v /test.ogg &
 
 while true
 do
   sleep 5
-  ogg123 -d alsa -v -o dev:plug:plugj /drip.ogg
+  ogg123 -d alsa -v /drip.ogg
 done
